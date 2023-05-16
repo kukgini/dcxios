45544b3db2043a6d102572669a347cb5861fe348
kukgini <kukgini@users.noreply.github.com>
2023-05-17 08:34:53 +0900

    Update DCX001.swift

--- dcxios/Views/Exam/DCX001.swift
+++ dcxios/Views/Exam/DCX001.swift
@@ -8,69 +8,81 @@ struct DCX001: View {
     
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text(model.greeting)
+            HStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Spacer()
+                Text(model.greeting)
+                Spacer()
+                ViewChangeButton(label: ">", view: .dcx002)
+            }
             
             List {
                 ForEach(1..<20) { index in
                     if index % 2 != 0 {
                         ListItemView(alignment: .left, title: "Title \(index)", description: "BlaBla... \(index)")
                     } else {
-                        ListItemView(alignment: .right, title: "Title \(index)", description: "BlaBla... \(index)")
+                        ListItemView(alignment: .right, title: "Title \(index)", description: "Description ... \(index)")
                     }
                 }
             }
             .listStyle(.plain)
             
-            ViewChangeButton(label: "Go DCX002 View", view: .dcx002)
-            Button(action: {
-                API.get(
-                    url: "https://httpbin.org/get",
-                    complition: { result in
-                        switch result {
-                        case .success(let json):
-                            print("\(json)")
-                        case .failure(let error):
-                            print("\(error.localizedDescription)")
-                        }
-                    })
-            }) {
-                Text("RestAPI GET Test")
-            }
-            Button(action: {
-                API.post(
-                    url: "https://httpbin.org/post",
-                    data: ["hello":"world"],
-                    complition: { result in
-                        switch result {
-                        case .success(let json):
-                            print("Hello \(json["json"]["hello"].stringValue)!")
-                        case .failure(let error):
-                            print("\(error.localizedDescription)")
-                        }
-                    })
-            }) {
-                Text("RestAPI POST Test")
+            HStack {
+                Button(action: {
+                    API.get(
+                        url: "https://httpbin.org/get",
+                        complition: { result in
+                            switch result {
+                            case .success(let json):
+                                print("\(json)")
+                            case .failure(let error):
+                                print("\(error.localizedDescription)")
+                            }
+                        })
+                }) {
+                    Text("API GET")
+                }
+                Text("|")
+                Button(action: {
+                    API.post(
+                        url: "https://httpbin.org/post",
+                        data: ["hello":"world"],
+                        complition: { result in
+                            switch result {
+                            case .success(let json):
+                                print("Hello \(json["json"]["hello"].stringValue)!")
+                            case .failure(let error):
+                                print("\(error.localizedDescription)")
+                            }
+                        })
+                }) {
+                    Text("API POST")
+                }
             }
             FavoriteButton(isSet: $isExpanded)
             Spacer()
-            CustomDisclosureGroup(isExpanded: $isExpanded) {
-                isExpanded.toggle()
-            } prompt: {
-                HStack(spacing: 0) {
-                    Text("How to open an account in your application?")
-                    Spacer()
-                    Text(">")
-                        .fontWeight(.bold)
-                        .rotationEffect(isExpanded ? Angle(degrees: 90) : .zero)
+            List {
+                ForEach(1..<5) { index in
+                    CustomDisclosureGroup(isExpanded: $isExpanded) {
+                        isExpanded.toggle()
+                    } prompt: {
+                        HStack(spacing: 0) {
+                            Text("How to open an account in your application?")
+                            Spacer()
+                            Text(">")
+                                .fontWeight(.bold)
+                                .rotationEffect(isExpanded ? Angle(degrees: 90) : .zero)
+                        }
+                        .padding(.horizontal, 20)
+                    } expandedView: {
+                        Text("you can open an account by choosing between gmail or ICloud when opening the application")
+                            .font(.system(size: 16, weight: .semibold, design: .monospaced))
+                    }
                 }
-                .padding(.horizontal, 20)
-            } expandedView: {
-                Text("you can open an account by choosing between gmail or ICloud when opening the application")
-                    .font(.system(size: 16, weight: .semibold, design: .monospaced))
             }
+            .listStyle(.plain)
         }
         .padding()
     }

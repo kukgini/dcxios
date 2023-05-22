900d7c26734fa1dbbf2324cf5282b915668db506
kukgini <kukgini@gmail.com>
2023-05-22 21:48:30 +0900

    add coding key as example

--- dcxios/Models/Shop.swift
+++ dcxios/Models/Shop.swift
@@ -9,4 +9,15 @@ public struct Shop: Codable, Identifiable {
     public var review: Int
     public var adFlag: String
     public var imageFile: String
+    
+    enum CodingKeys : String, CodingKey{
+            case id
+            case category
+            case name
+            case point
+            case review
+            case adFlag
+            case imageFile
+            //case birthday = "birth_date"
+        }
 }

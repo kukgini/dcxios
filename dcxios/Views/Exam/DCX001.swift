dd4e3e94d1afd55ae5a027b4882bf20b3cd70cae
kukgini <kukgini@gmail.com>
2023-06-01 14:12:12 +0900

    update view in more SwiftUI way.

--- dcxios/Views/Exam/DCX001.swift
+++ dcxios/Views/Exam/DCX001.swift
@@ -22,17 +22,6 @@ struct SortButton: View {
     }
 }
 
-struct GreetingView: View {
-    var greeting: String
-    var body: some View {
-        HStack(alignment: .center) {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text(greeting)
-        }
-    }
-}
 struct NameFilterField: View {
     var label: String
     @Binding var filter: String
@@ -99,30 +88,27 @@ struct DCX001: View {
     @State var isExpanded: Bool
     
     var body: some View {
-        VStack {
-            GreetingView(greeting: model.greeting)
-
-            VStack(alignment: .center) {
-                HStack(alignment: .center) {
-                    CategoryFilterButton(label: "전체", filter: $model.categoryFilter, category: nil)
-                    CategoryFilterButton(label: "치킨", filter: $model.categoryFilter, category: .chicken)
-                    CategoryFilterButton(label: "피자", filter: $model.categoryFilter, category: .pizza)
-                    CategoryFilterButton(label: "분식", filter: $model.categoryFilter, category: .bunsik)
-                    CategoryFilterButton(label: "카페", filter: $model.categoryFilter, category: .caffee)
-                }
-                HStack(alignment: .center) {
-                    SortButton(label: "기본 정렬순", option: $model.sortOption, key: .basic)
-                    SortButton(label: "별점 높은순", option: $model.sortOption, key: .point)
-                    SortButton(label: "리뷰 많은순", option: $model.sortOption, key: .review)
-                }
-                HStack {
-                    NameFilterField(label: "Shop Name", filter: $model.nameFilter)
-                    FavoriteButton(isSet: $model.adFilter)
-                }
-            }
-            .padding()
-            
+        NavigationView {
             List {
+                VStack(alignment: .center) {
+                    HStack(alignment: .center) {
+                        CategoryFilterButton(label: "전체", filter: $model.categoryFilter, category: nil)
+                        CategoryFilterButton(label: "치킨", filter: $model.categoryFilter, category: .chicken)
+                        CategoryFilterButton(label: "피자", filter: $model.categoryFilter, category: .pizza)
+                        CategoryFilterButton(label: "분식", filter: $model.categoryFilter, category: .bunsik)
+                        CategoryFilterButton(label: "카페", filter: $model.categoryFilter, category: .caffee)
+                    }
+                    HStack(alignment: .center) {
+                        SortButton(label: "기본 정렬순", option: $model.sortOption, key: .basic)
+                        SortButton(label: "별점 높은순", option: $model.sortOption, key: .point)
+                        SortButton(label: "리뷰 많은순", option: $model.sortOption, key: .review)
+                    }
+                    HStack {
+                        NameFilterField(label: "Shop Name", filter: $model.nameFilter)
+                        FavoriteButton(isSet: $model.adFilter)
+                    }
+                }
+                
                 ForEach(model.filteredShops) { shop in
                     Button(action: {
                         model.shop = shop
@@ -131,11 +117,7 @@ struct DCX001: View {
                         ShopView(shop: shop)
                     }
                 }
-            }
-            .listStyle(.plain)
-//            .padding()
-            
-            List {
+                
                 ForEach(1..<10) { index in
                     if index % 2 != 0 {
                         ListItemView(alignment: .left, title: "Title \(index)", description: "BlaBla... \(index)")
@@ -143,80 +125,81 @@ struct DCX001: View {
                         ListItemView(alignment: .right, title: "Title \(index)", description: "BlaBla... \(index)")
                     }
                 }
-            }
-            .listStyle(.plain)
-//            .padding()
-            
-            HStack {
-                Button(action: {
-                    API.get(
-                        url: "https://httpbin.org/get",
-                        complition: { result in
-                            switch result {
-                            case .success(let json):
-                                print("\(json)")
-                            case .failure(let error):
-                                print("\(error.localizedDescription)")
-                            }
-                        })
-                }) {
-                    Text("GET")
-                }
-                Text("|")
-                Button(action: {
-                    API.post(
-                        url: "https://httpbin.org/post",
-                        data: ["hello":"world"],
-                        complition: { result in
-                            switch result {
-                            case .success(let json):
-                                print("Hello \(json["json"]["hello"].stringValue)!")
-                            case .failure(let error):
-                                print("\(error.localizedDescription)")
-                            }
-                        })
-                }) {
-                    Text("POST")
-                }
-                Text("|")
-                Button(action: {
-                    API.get(
-                        url: "http://localhost:3000/dcx/1/shopList",
-                        complition: { result in
-                            switch result {
-                            case .success(let json):
-                                do {
-                                    print(json["shopList"])
-                                    model.shopList = try decode(json["shopList"].rawData())
-                                } catch {
-                                    print("GET shopList failed. \(error)")
+                
+                HStack {
+                    Button(action: {
+                        API.get(
+                            url: "https://httpbin.org/get",
+                            complition: { result in
+                                switch result {
+                                case .success(let json):
+                                    print("\(json)")
+                                case .failure(let error):
+                                    print("\(error.localizedDescription)")
                                 }
-                            case .failure(let error):
-                                print("\(error.localizedDescription)")
-                            }
-                        })
-                }) {
-                    Text("SHOP LIST")
+                            })
+                    }) {
+                        Text("GET")
+                    }
+                    Text("|")
+                    Button(action: {
+                        API.post(
+                            url: "https://httpbin.org/post",
+                            data: ["hello":"world"],
+                            complition: { result in
+                                switch result {
+                                case .success(let json):
+                                    print("Hello \(json["json"]["hello"].stringValue)!")
+                                case .failure(let error):
+                                    print("\(error.localizedDescription)")
+                                }
+                            })
+                    }) {
+                        Text("POST")
+                    }
+                    Text("|")
+                    Button(action: {
+                        API.get(
+                            url: "http://localhost:3000/dcx/1/shopList",
+                            complition: { result in
+                                switch result {
+                                case .success(let json):
+                                    do {
+                                        print(json["shopList"])
+                                        model.shopList = try decode(json["shopList"].rawData())
+                                    } catch {
+                                        print("GET shopList failed. \(error)")
+                                    }
+                                case .failure(let error):
+                                    print("\(error.localizedDescription)")
+                                }
+                            })
+                    }) {
+                        Text("SHOP LIST")
+                    }
+                }
+                Spacer()
+                CustomDisclosureGroup(isExpanded: $isExpanded) {
+                    isExpanded.toggle()
+                } prompt: {
+                    HStack(spacing: 0) {
+                        Text("How to open an account in your application?")
+                        Spacer()
+                        Text(">")
+                            .fontWeight(.bold)
+                            .rotationEffect(isExpanded ? Angle(degrees: 90) : .zero)
+                    }
+                    .padding(.horizontal, 20)
+                } expandedView: {
+                    Text("you can open an account by choosing between gmail or ICloud when opening the application")
+                        .font(.system(size: 16, weight: .semibold, design: .monospaced))
                 }
             }
-            Spacer()
-            CustomDisclosureGroup(isExpanded: $isExpanded) {
-                isExpanded.toggle()
-            } prompt: {
-                HStack(spacing: 0) {
-                    Text("How to open an account in your application?")
-                    Spacer()
-                    Text(">")
-                        .fontWeight(.bold)
-                        .rotationEffect(isExpanded ? Angle(degrees: 90) : .zero)
-                }
-                .padding(.horizontal, 20)
-            } expandedView: {
-                Text("you can open an account by choosing between gmail or ICloud when opening the application")
-                    .font(.system(size: 16, weight: .semibold, design: .monospaced))
-            }
+            .navigationBarTitle("DCX001")
+            .navigationBarItems(
+                leading: Text("A"),
+                trailing: Text("B"))
         }
-        .padding()
     }
 }
 
